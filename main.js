750a17ab4d0a79f37d833b0562aba7314a3e6008
Marcoo26 <marcobarcos24@gmail.com>
2024-08-13 05:52:20 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Cuma mau bilang", "Selamat Pagii Sayangku", "Aku Sayang Banget Sama Kamu", "Semoga Aktifitas Kamu Hari ini Lancar yaa","I Love You Ashya Ayu Pangestia","Dah sii itu aja hhehe...","Ary Sayang Bangettt sama Ashya"],
+        typeSpeed: 80
+    });
+}
